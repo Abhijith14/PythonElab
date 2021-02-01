538e174ac55c206ce4f527b588c8685e491aaad2
ABHIJITH UDAYAKUMAR <53612189+Abhijith14@users.noreply.github.com>
2021-02-01 23:56:12 +0530

    Commit

---
+++ DisplaytheuserAddress.py
@@ -0,0 +1,18 @@
+#Q. 1:
+
+#Display the user Address(Input and Output)
+
+#Write a program to read the address of a user. Display the result by breaking it into multiple lines
+#Input and Output Format:
+#Refer sample input and output for formatting specification.
+#All float values are displayed correct to 2 decimal places.
+#All text in bold corresponds to input and the rest corresponds to output
+
+
+n = input()
+a = input()
+p = input()
+print("Address")
+print("House Number:",n)
+print("Area:",a)
+print("Pin Code:",p)
