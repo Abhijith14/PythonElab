124e971363ead318694e92de21d2cb3e32809e71
ABHIJITH UDAYAKUMAR <53612189+Abhijith14@users.noreply.github.com>
2021-02-03 00:00:08 +0530

    Commit

---
+++ SumoftheSeries.py
@@ -0,0 +1,15 @@
+#Q. 3:
+
+#Sum of the Series(Input and Output)
+
+#Write a program to find sum of series
+#Hint:1+2+3+4+n
+
+
+n = int(input())
+i = 1
+s = 0
+while i <= n:
+  s = s + i
+  i+=1
+print(s)
