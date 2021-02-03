fe848340c81a3394d4787d1f60f38e8143307c49
ABHIJITH UDAYAKUMAR <53612189+Abhijith14@users.noreply.github.com>
2021-02-04 00:00:08 +0530

    Commit

---
+++ 100Years.py
@@ -0,0 +1,12 @@
+#Q. 4:
+
+#100 Years(Input and Output)
+
+#Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year  they will turn 100 years old.
+
+
+curr_Y = 2017
+name = input()
+age = int(input())
+diff = 100 - age
+print(name,"will be 100 years old in",curr_Y + diff)
