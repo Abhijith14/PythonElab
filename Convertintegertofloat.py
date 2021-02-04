c2a3c075e1d85317f4f4a59a0f7d45320fadf177
ABHIJITH UDAYAKUMAR <53612189+Abhijith14@users.noreply.github.com>
2021-02-05 00:00:08 +0530

    Commit

---
+++ Convertintegertofloat.py
@@ -0,0 +1,11 @@
+#Q. 5:
+
+#Convert integer to float(Input and Output)
+
+#Given an input as integer to the machine , he need to get the output of corresponding floating point number
+#Hint: Divide the number by 100 and display the output
+#Please help him to write the program that satisfy the output.
+
+
+n = int(input())
+print(n/100)
