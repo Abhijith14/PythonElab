c9c5db11703f2a2793bb4e7266048544b1bbe53e
ABHIJITH UDAYAKUMAR <53612189+Abhijith14@users.noreply.github.com>
2021-02-06 00:00:09 +0530

    Commit

---
+++ Traingle.py
@@ -0,0 +1,12 @@
+#Q. 6:
+
+#Traingle(Input and Output)
+
+#The area of a triangle can be computed using the following formula, where b is the length of the base of the triangle, and h is its height:
+#area = b* h/2
+#Write a program that allows the user to enter values for b and h. The program should then compute and display the area of a triangle with base length b and height h.
+
+
+b = int(input())
+h = int(input())
+print("The area of the triangle is",b*h/2)
