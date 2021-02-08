9b1091df770c4d38a738d9c861f77b10bbfdb8e3
ABHIJITH UDAYAKUMAR <53612189+Abhijith14@users.noreply.github.com>
2021-02-09 00:00:08 +0530

    Commit

---
+++ DisplayIt.py
@@ -0,0 +1,16 @@
+#Q. 9:
+
+#Display It(Input and Output)
+
+#Create a program that displays your name and complete mailing address formatted in the manner that you would usually see it on the outside of an envelope. 
+#Your program needs to read any input from the user
+
+
+name = input()
+mail = input()
+city = input()
+no = input()
+print(name)
+print(mail)
+print(city)
+print(no)
