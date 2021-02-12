22a262595afe0328b4652ba53a638be380832246
ABHIJITH UDAYAKUMAR <53612189+Abhijith14@users.noreply.github.com>
2021-02-13 00:00:08 +0530

    Commit

---
+++ MirrorImage2.py
@@ -0,0 +1,12 @@
+#Q. 13:
+
+#Mirror Image 2(Control Stmt - if-elif-el)
+
+#Puck, the trickster, has again started troubling people in your city. The people have turned on to you for getting rid of Puck. 
+#Puck presents to you a number consisting of  numbers from  0 to 9 characters. 
+#He wants you to count the numbers that he uttered. 
+#Kindly  help people to get rid of the Puck by developing a small program .The hope of people are on you so you have to solve the riddle
+
+
+num = input()
+print(len(num))
