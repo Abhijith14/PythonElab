d3fe1e2397314d56b2f6974b42fcccf84643ec88
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-02-21 00:00:09 +0530

    Commit

---
+++ FACTORS.py
@@ -0,0 +1,13 @@
+#Q. 21:
+
+#FACTORS(Control Stmt - for-while)
+
+#The program finds the factors of a number, which means the other numbers which can divide the given number.       
+#Eg: 5 is divisible by 5 and 1
+
+
+num = int(input())
+for i in range(2,num+1):
+  if num%i == 0:
+    print(i)
+print("1")
