5bb20e491e98c04df96b6c4348a2d246723c0fb6
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-02-22 00:00:08 +0530

    Commit

---
+++ PerfectNumber.py
@@ -0,0 +1,19 @@
+#Q. 22:
+
+#Perfect Number(Control Stmt - for-while)
+
+#Write a program to check whether the given number is perfect number or not
+#Input:A positive number
+#Output:Display the appropriate message 
+#Refer sample input and output for formatting specification.
+
+
+num = int(input())
+s = 0
+for i in range(1,num):
+  if num % i == 0:
+    s = s + i
+if s == num:
+  print("The number is a Perfect number")
+else:
+  print("The number is not a Perfect number")
