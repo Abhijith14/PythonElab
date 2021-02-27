da680303138e64f79f726309483b81d3b1ec0e61
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-02-28 00:00:06 +0530

    Commit

---
+++ Exponentation-PartB.py
@@ -0,0 +1,20 @@
+#Q. 28:
+
+#Exponentation- Part B(Control Stmt - for-while)
+
+#Write a program to find the exponentation of given number
+#Input 1: Base
+#Input 2: Number of inputs
+#Output:
+#List of exponentation terms for the given input
+
+
+exp = int(input())
+no = int(input())
+print("The total terms is:",no)
+i,s = 1,1
+print(1)
+while i <= no:
+  s = s*exp
+  print(s)
+  i = i + 1
