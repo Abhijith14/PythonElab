8182f07082284c0c890a99110520d2ddb8c6a518
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-03-08 00:00:08 +0530

    Commit

---
+++ CountMepleasewithspaces.py
@@ -0,0 +1,26 @@
+#Q. 36:
+
+#Count Me please with spaces(Strings)
+
+#Write a Python program to count the number of lower-case letter, uppercase letters and spaces in the entered string.
+#Refer sample input and output for formatting specification.
+#All float values are displayed correct to 2 decimal places.
+#All text in bold corresponds to input and the rest corresponds to output
+#Output:
+#First Line: Lowercase count
+#Second Line: Uppercase count
+#Third Line: Spaces count in the string
+
+
+string = input()
+l = u = s = 0
+for i in range(len(string)):
+  if string[i] == " ":
+    s = s + 1
+  elif str.isupper(string[i]):
+    u = u + 1
+  elif str.islower(string[i]):
+    l = l + 1
+print(l)
+print(u)
+print(s)
