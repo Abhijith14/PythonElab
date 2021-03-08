ec76be94d0d1cf7e02bf26297e36209ee5222aa5
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-03-09 00:00:08 +0530

    Commit

---
+++ Whoisleasthere.py
@@ -0,0 +1,16 @@
+#Q. 37:
+
+#Who is least here(Strings)
+
+#Write a Python program to count the number of lower-case letters in the entered string.
+#Refer sample input and output for formatting specification.
+#All float values are displayed correct to 2 decimal places.
+#All text in bold corresponds to input and the rest corresponds to output.
+
+
+string = input()
+c = 0
+for i in range(len(string)):
+  if str.islower(string[i]):
+    c = c + 1
+print(c)
