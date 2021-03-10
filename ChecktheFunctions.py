1732997773a0076b6824c4f2352c9ca7664e586f
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-03-11 00:00:08 +0530

    Commit

---
+++ ChecktheFunctions.py
@@ -0,0 +1,30 @@
+#Q. 39:
+
+#Check the Functions(Strings)
+
+#Write a program to check whether the given string contains the following:
+#1. input.isalpha()   = check if all char in the string are alphabetic
+#2. input.isdigit()     = test if string contains digits
+#3. input.istitle()      = test if string contains title words
+#4. input.isupper()  = test if string contains upper case
+#5. input.islower()   = test if string contains lower case
+
+
+string = input()
+a = b = c = d = e = False 
+for i in range(len(string)):
+  if str.isalpha(string[i]):
+    a = True
+  if str.isdigit(string[i]):
+    b= True
+  elif str.istitle(string):
+    c = True
+  elif str.isupper(string[i]):
+    d = True
+  elif str.islower(string[i]):
+    e = True
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
