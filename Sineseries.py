5885c050aefcd393041566463f35d2d5f2585995
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-03-14 00:00:08 +0530

    Commit

---
+++ Sineseries.py
@@ -0,0 +1,24 @@
+#Q. 42:
+
+#Sine series(Functions)
+
+#Python Program to Find the Sum of Sine Series
+#Input
+#First Line : The value of x in degrees
+#Second Line: The number of terms
+#Output
+#Print the Sum of Sine series
+
+
+import math
+def sin(x,n):
+    s = 0
+    for i in range(n):
+        sign = (-1)**i
+        pi=22/7
+        y=x*(pi/180)
+        s = s + ((y**(2.0*i+1))/math.factorial(2*i+1))*sign
+    return s
+x=int(input())
+n=int(input())
+print(round(sin(x,n),2))
