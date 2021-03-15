3d1fe23413f3ee07e18aad3d49ca5229cc3fda4c
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-03-16 00:00:06 +0530

    Commit

---
+++ NaturalNumbers.py
@@ -0,0 +1,16 @@
+#Q. 44:
+
+#Natural Numbers(Functions)
+
+#Python Program to Find the Sum of First N Natural Numbers
+#Input 
+#Value of "N"
+#Output
+#Print the Sum of N natural Numbers
+
+
+N = int(input())
+s = 0
+for i in range(1,N+1):
+  s = s + i
+print(s)
