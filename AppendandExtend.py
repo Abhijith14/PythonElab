5c27394fb066fb50fd9cf38f2a8c5aa1c0f84b3f
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-03-23 00:00:06 +0530

    Commit

---
+++ AppendandExtend.py
@@ -0,0 +1,26 @@
+#Q. 51:
+
+#Append and Extend(Lists and Tuples)
+
+#Write a program to append two list and display the extended or appended list
+#Input:
+#1. The Size of First List
+#2. The Size of Second List
+#3. First List elements
+#4. Second List elements
+#Output:
+#Appended Single List
+#Refer sample input and output for formatting specification. 
+#All float values are displayed correct to 2 decimal places. 
+#All text in bold corresponds to input and the rest corresponds to output.
+
+
+s1 = int(input())
+s2 = int(input())
+l1 = list()
+l2 = list()
+for i in range(s1):
+  l1.append(int(input()))
+for i in range(s2):
+  l2.append(int(input()))
+print(l1+l2)
