b9328df2b602b27a0d1f760d74b9f1496ed57671
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-03-29 20:06:10 +0530

    Commit

---
+++ WordSort(36).py
@@ -0,0 +1,18 @@
+#Q. 30:
+
+#Word Sort(Control Stmt - for-while)
+
+#Write a program to sort the words in the sentence in alphabetic order
+#Input:A Sentence 
+#Output: Display the words of a sentence in sorted order 
+#Refer sample input and output for formatting specification.
+
+
+string = input()
+data = {}
+lst = list(string.split(" "))
+for i in range(len(lst)):
+  data.update({lst[i][0]:lst[i]})
+print("The sorted words are")
+for i in sorted (data.keys()):
+  print(data[i]) 
