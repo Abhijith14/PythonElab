e4d92d2f7469dde55472e740c41c9ccb2c7cbaca
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-12 00:00:07 +0530

    Commit

---
+++ IdentityMatrix(1).py
@@ -0,0 +1,21 @@
+#Q. 70:
+
+#Identity Matrix(Matrix  Operations)
+
+#Write Python Program to read a number n and print an identity matrix of the desired size.
+#Input:
+#Size of the matrix
+#Output:
+#Print the Identity matrix
+
+
+n = int(input())
+for i in range(0,n):
+    for j in range(0,n):
+        if(i==j):
+            print("1",sep = " ",end = " ")
+        else:
+            print("0",sep = " ",end = " ")
+    print()
+     
+  
