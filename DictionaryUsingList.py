727f249786804827d210e30378e4b9212c76c86a
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-13 00:00:08 +0530

    Commit

---
+++ DictionaryUsingList.py
@@ -0,0 +1,28 @@
+#Q. 71:
+
+#Dictionary Using List(Dictionary)
+
+#Write a program to get the input of key and value of the element in dictionary.
+#Display the key-value pair in the python dictionary format.
+#Kindle get the input using List Concept.
+#Input:
+#1. Size of first list
+#2. Elements of first list
+#3. Size of second list
+#4. Elements of second list
+#Output:
+#1. The first list elements are keys of the dictionaries
+#2. The second list elements are corresponding elements of the dictionaries
+
+
+s1 = int(input())
+L1 = []
+L2 = []
+for i in range(s1):
+  L1.append(int(input()))
+s2 = int(input())
+for i in range(s2):
+  L2.append(int(input()))
+
+D = dict(zip(L1,L2))
+print(D)
