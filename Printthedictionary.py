3dfdaaadac4932fbe1bf499ec4f11e8c79559031
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-14 15:39:04 +0530

    Commit

---
+++ Printthedictionary.py
@@ -0,0 +1,16 @@
+#Q. 72:
+
+#Print the dictionary(Dictionary)
+
+#Write a program to get the input of key and value of the element in dictionary.
+#Display the key-value pair in the python dictionary format.
+#Refer sample input and output for formatting specification. 
+#All float values are displayed correct to 2 decimal places. 
+#All text in bold corresponds to input and the rest corresponds to output.
+
+
+key = int(input())
+D = {}
+D[key] = int(input())
+print("Updated dictionary is:")
+print(D)
