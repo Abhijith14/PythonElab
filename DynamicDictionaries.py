cdf9bd7a2101f85f8cb39a045a9210dc4d4c1b94
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-19 00:00:06 +0530

    Commit

---
+++ DynamicDictionaries.py
@@ -0,0 +1,19 @@
+#Q. 77:
+
+#Dynamic Dictionaries(Dictionary)
+
+#Write a program to print all the squares of the range from 1 to n
+#Input:
+#1.Get the Value
+#Output:
+#1. Display the square value of the dictionary
+#Refer sample input and output for formatting specification. 
+#All float values are displayed correct to 2 decimal places. 
+#All text in bold corresponds to input and the rest corresponds to output
+
+
+k = int(input())
+D = {}
+for i in range(1,k+1):
+  D[i] = i**2
+print(D)
