9d74dee6f1d50543859d933517f38ffdaad41a2d
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-23 00:00:10 +0530

    Commit

---
+++ Findingtheoccurrenceofaninteger.py
@@ -0,0 +1,13 @@
+#Q. 81:
+
+#Finding the occurrence of an integer(Searching and Sorting)
+
+#Find the given integer in the given list of integers  and print the number of occurrences of that integer in the list.
+
+
+s = int(input())
+L = []
+for i in range(s):
+  L.append(int(input()))
+key = int(input())
+print(L.count(key))
