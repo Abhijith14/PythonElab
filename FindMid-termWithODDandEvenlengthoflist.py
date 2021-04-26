8ebadd14098780760f6dfd5b695c2a23cb5d7d7d
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-27 01:26:15 +0530

    Commit

---
+++ FindMid-termWithODDandEvenlengthoflist.py
@@ -0,0 +1,20 @@
+#Q. 85:
+
+#Find Mid-term With ODD and Even length of list(Searching and Sorting)
+
+#Perform sorting on list of integers and print the mid-term.
+
+
+s = int(input())
+L = list()
+for i in range(s):
+  L.append(int(input()))
+L.sort()
+print("Sorted List:")
+print(L)
+print("Mid-term:")
+n = int(s/2)
+if s % 2 == 0:
+  print(L[n-1])
+else:
+  print(L[n])
