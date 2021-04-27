6d913df9011859211e58f67a06893d24c851087e
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-28 01:50:51 +0530

    Commit

---
+++ Findincrementsequence.py
@@ -0,0 +1,20 @@
+#Q. 86:
+
+#Find increment sequence(Searching and Sorting)
+
+#Perform sorting on list of integers and print the sequence showing order of increment.
+
+
+s = int(input())
+t = input().split()
+L = []
+ans = []
+for i in range(s):
+  L.append(int(t[i]))
+L.sort()
+print("Sorted List:")
+print(L)
+for i in range(s-1):
+  ans.append(L[i+1]-L[i])
+print("Sequence of increments:")
+print(ans)
