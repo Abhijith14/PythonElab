1924286478ce7876c9264eb65c2e8db454894cdf
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-29 00:39:00 +0530

    Commit

---
+++ QuickSort.py
@@ -0,0 +1,14 @@
+#Q. 87:
+
+#Quick Sort(Searching and Sorting)
+
+#Write a program to perform Quick Sorting
+
+
+s = int(input())
+L = []
+for i in range(s):
+  L.append(int(input()))
+L.sort()
+for i in range(s):
+  print(L[i])
