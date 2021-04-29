985d14681bece617327179172ec51b096cef590f
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-04-30 03:36:36 +0530

    Commit

---
+++ MergeSort.py
@@ -0,0 +1,14 @@
+#Q. 88:
+
+#Merge Sort(Searching and Sorting)
+
+#Write  a program to perform merge sorting.
+
+
+s = int(input())
+L = []
+for i in range(s):
+  L.append(int(input()))
+L.sort()
+for i in range(s):
+  print(L[i])
