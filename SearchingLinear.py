7c1258b17f0f22a4944b27dbb374d147fda6cb3b
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-05-02 00:00:07 +0530

    Commit

---
+++ SearchingLinear.py
@@ -0,0 +1,15 @@
+#Q. 90:
+
+#Searching Linear(Searching and Sorting)
+
+#Write a program to search a element linearly
+
+
+s = int(input())
+L = []
+for i in range(s):
+  L.append(int(input()))
+key = int(input())
+for i in range(s):
+  if L[i] == key:
+    print("Element is present at index",i)
