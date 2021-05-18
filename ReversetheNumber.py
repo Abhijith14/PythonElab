08e5abe727eb31effbd5053ed86aeaa0fce83a68
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-05-19 00:00:09 +0530

    Commit

---
+++ ReversetheNumber.py
@@ -0,0 +1,52 @@
+#Q. 7:
+
+#Reverse the Number(Input and Output)
+
+#Dark was deeply studying in the afternoon and came across a concept called as "REVERSE OF A NUMBER". He decided to design a program which can reverse any long numbers.
+#At the end of afternoon, he started to code and finally end up with no results, so decided to ask you all for the help, he wanted you to design a very efficient method to reverse a number and say it as a EVEN or ODD.
+#Confused, Simply print "EVEN"(without quotes) if the reverse of the given number is even and print "ODD"(without quotes) if the reverse of the given number is odd.
+#Input:
+#The first line contains T, the number of test cases. Followed by T lines each contains a single number.
+#Output:
+#For every test case print the "EVEN"(without quotes) if reverse of GIVEN number is even else print "ODD"(without quotes).
+#Constraints:
+#1<=T<=10000
+#1<=N<=10100
+#Sample Input: 
+#4 
+#1
+#3
+#6
+#15
+#Sample Output:
+#ODD
+#ODD 
+#EVEN
+#ODD
+
+
+'''def rev(num):
+  L = len(str(num))
+  s = 0
+  if L == 1:
+    i = 10
+  elif L == 2:
+    i = 100
+  elif L == 3:
+    i = 1000
+  while(num > 0):
+    i = i / 10
+    rem = num % 10
+    s = s + int(rem*i)
+    num = num // 10
+  print(s)'''
+
+T = int(input())
+N = []
+for i in range(T):
+  N.append(int(input()))
+for i in range(T):
+  if N[i] % 2 == 0:
+    print("EVEN")
+  else:
+    print("ODD")
